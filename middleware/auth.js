d5c59e0f317834e63d6576a9586955e9a08cc76f
sridhar <sridzar1234@gmail.com>
2024-05-19 22:57:12 +0530

    final commmit

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ const auth = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token,'your_jwt_secret_key');
         req.user = decoded;
         next();
     } catch (err) {
